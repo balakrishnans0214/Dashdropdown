b6de86d144e89c4f27ca2808b0c6fdc1273807b6
balakrishnans0214 <54431840+balakrishnans0214@users.noreply.github.com>
2019-12-11 15:25:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,74 +1,116 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
-import dash
-from dash.dependencies import Input, Output
-import dash_html_components as html
-import dash_core_components as dcc
-import plotly.graph_objs as go
-
-app = dash.Dash(__name__)
-server = app.server
-
-df = pd.read_csv('/home/client/Videos/Project/All.csv')
-names = df.columns[1:-1]
-available_indicators = df['Sector'].unique()
-
-app.layout = html.Div(
-    [
-        html.Div([
-            dcc.Dropdown(
-                id='drop',
-                options=[{'label': i, 'value': i} for i in available_indicators],
-                value='Energy',
-                style={'width': '50%'}
-            ),
-            dcc.Dropdown(
-                id='ddl_x',
-                options=[{'label': i, 'value': i} for i in names],
-                value='Price',
-                style={'width': '50%'}
-            ),
-            dcc.Dropdown(
-                id='ddl_y',
-                options=[{'label': i, 'value': i} for i in names],
-                value='Market Cap',
-                style={'width': '50%'}
-            ),
-        ], style={'width': '100%', 'display': 'inline-block'}),
-        html.Div([
-            dcc.Graph(id='graph1')
-        ], style={'width': '100%', 'display': 'inline-block'})
-    ]
-)
 
 
-@app.callback(
-    Output(component_id='graph1', component_property='figure'),
-    [
-        Input(component_id='drop', component_property='value'),
-        Input(component_id='ddl_x', component_property='value'),
-        Input(component_id='ddl_y', component_property='value')
-    ]
-)
-def update_output(drop, ddl_x_value, ddl_y_value):
-    dff = df[df.Sector.str.contains(drop)]
-
-    figure = {
-        'data': [
-            go.Scatter(
-                x=dff[ddl_x_value],
-                y=dff[ddl_y_value],
-            )
-        ],
-        'layout':
-            go.Layout(
-                height=350,
-                hovermode='closest',
-                title=go.layout.Title(text='Dash Interactive Data Visualization', xref='paper', x=0)
-            )
-
-    }
-    return figure
 
 
-if __name__ == '__main__':
-    app.run_server(debug=True, port=8080)
+# In[21]:
+
+
+df = pd.read_csv('/home/client/Downloads/1minstock.csv')
+df.head()
+
+
+# In[22]:
+
+
+Ticker = list(set(df.Ticker))
+len(Ticker)
+
+
+# In[23]:
+
+
+Ticker[:30]
+
+
+# In[24]:
+
+
+# selecting YHOO stocks
+df = df[df['Ticker']=='ICICIBANK.NSE']
+
+# preparing input features
+df = df.drop(['Ticker'], axis=1)
+
+
+# In[25]:
+
+
+df
+
+
+# In[26]:
+
+
+X = df[['Open', 'High', 'Low', 'Volume']].values
+y = df['Close'].values
+
+
+# In[27]:
+
+
+import xgboost
+
+# XGboost algorithm
+xgb = xgboost.XGBRegressor(objective ='reg:squarederror')
+
+
+# In[28]:
+
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+xgb.fit(X_train,y_train)
+
+
+# In[29]:
+
+
+from sklearn.metrics import explained_variance_score
+
+predictions = xgb.predict(X_test)
+print(explained_variance_score(predictions,y_test))
+
+
+# In[30]:
+
+
+# from sklearn.preprocessing import Imputer
+from sklearn.impute import SimpleImputer
+
+# Preprocessing data
+my_imputer = SimpleImputer()
+X_train = my_imputer.fit_transform(X_train)
+X_test = my_imputer.transform(X_test)
+
+
+# In[31]:
+
+
+from xgboost import XGBRegressor
+
+model = XGBRegressor(objective ='reg:squarederror')
+# Add silent=True to avoid printing out updates with each cycle
+model.fit(X_train, y_train, verbose=True)
+
+
+# In[32]:
+
+
+# make predictions
+predictions = model.predict(X_test)
+
+from sklearn.metrics import mean_absolute_error
+print("Mean Absolute Error : " + str(mean_absolute_error(predictions, y_test)))
+
+
+# In[33]:
+
+
+plt.plot(predictions,color = 'blue', label='Predicted')
+plt.plot(y_test,color = 'red', label='Ground Truth')
+plt.legend()
+plt.show()
